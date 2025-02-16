91ad3809aff9cbaca029d999a4cff80d6b101528
Victor Veras <victorveras@alu.ufc.br>
2025-02-16 17:53:48 -0300

    banner de anuncios com js funcional na home

--- dist/scripts/carrossel.js
+++ dist/scripts/carrossel.js
@@ -1,10 +1,12 @@
 let counter = 1;
 
-setInterval(() => {
-    document.getElementById('slide' + counter).checked = true;
+function autoSlide() {
+    document.getElementById("radio" + counter).checked = true;
     counter++;
 
     if (counter > 3) {
         counter = 1;
     }
-}, 4000); // Muda o slide a cada 4 segundos
+}
+
+setInterval(autoSlide, 5000); // Troca a cada 5 segundos
