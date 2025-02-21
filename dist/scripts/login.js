78bc46e754f1b1fdc7f36f82ae94f5d6a9b92630
ryanguilherme <ryanguilhermetbt@gmail.com>
2025-02-21 20:32:14 -0300

    refactor(.): diretorios modificados

--- dist/scripts/login.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const Login = document.querySelector("form");
-Login.addEventListener("Submit", function (event) {
-    if (!Login.checkValidity() || Login.checkValidity() == null) {
-        alert("Porfavor, preencha todos os campos corretamente!");
-        return alert;
-    }
-});
